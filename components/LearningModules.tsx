d5c81bd622b4f3598743f090675ce5300f15479c
bucopi <marcjames.beltran@g.batstate-u.edu.ph>
2024-12-17 14:35:15 +0800

    commit

--- components/LearningModules.tsx
+++ components/LearningModules.tsx
@@ -11,7 +11,7 @@ const LearningModules = () => {
           title="STEAM"
           description="This module covers the basics of IoT technology, including sensors, actuators, and communication protocols."
           imageSrc="/samples/MODULE (2).png"
-          learnRoute="/learn/steam"
+          learnRoute="/steam-learn"
         />
         <ModuleCard
           title="Agri-Aqua"
