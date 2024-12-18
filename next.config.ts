4ff53692ad038638ada65c2358ed287118750455
bucopi <marcjames.beltran@g.batstate-u.edu.ph>
2024-12-18 14:37:01 +0800

    update

--- next.config.ts
+++ next.config.ts
@@ -2,14 +2,19 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   transpilePackages: ["three"],
-  remotePatterns: [
-    {
-      protocol: "https",
-      hostname: "www.youtube.com",
-      port: "",
-      pathname: "/**",
-    }
-  ]
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "www.youtube.com",
+        port: "",
+        pathname: "/**",
+      }
+    ]
+  },
+  experimental: {
+    turbo: false
+  }
 };
 
 export default nextConfig;
