0700ed6ff9b0cb1bc707c8c97f6d0547935b392a
bucopi <marcjames.beltran@g.batstate-u.edu.ph>
2024-12-18 14:06:24 +0800

    push

---
+++ app/steam-learn/basic-electronics/circuit-analysis/quiz/page.tsx
@@ -0,0 +1,12 @@
+'use client'
+
+import { Button } from "@/components/ui/button"
+
+export default function QuizPage() {
+  return (
+    <div className="container mx-auto p-4">
+      <h1 className="text-2xl font-bold mb-4">Circuit Analysis Quiz</h1>
+      <Button>Start Quiz</Button>
+    </div>
+  )
+}
